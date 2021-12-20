e2b7a310d511caa5cbf2a9f52a87a669aa2a79ce
Kahlil Ashanti <kahlil.ashanti@gmail.com>
2021-12-20 12:20:46 -0800

    added two more tests

--- test/NewToken.test.js
+++ test/NewToken.test.js
@@ -12,6 +12,10 @@ chai.use(chaiAsPromised);
 var expect = chai.expect;
 
 contract("Token Test", async (accounts) => {
+
+    //define accounts
+    const [deployerAccount, recipient, anotherAccount] = accounts;
+
     it("all tokens should be in my account", async () => {
         //here we define our test case
         let instance = await Token.deployed();
@@ -19,7 +23,32 @@ contract("Token Test", async (accounts) => {
         // let balance = await instance.balanceOf(accounts[0]);
         // assert.equal(balance.valueOf(), initalSupply.valueOf(), "the balance was not the same")
         //it is better to use expect because it is more readable
-        expect(instance.balanceOf(accounts[0])).to.eventually.be.bignumber.equal(totalSupply);
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.bignumber.equal(totalSupply);
         //to run the test run truffle developer then then type test
+    })
+
+    it("is possible to send tokens between accounts", async () => {
+        //we are going to send a token from the deployer account to the recipient account and see if the balance changed
+        //send one token
+        const sendTokens = 1;
+        let instance = await Token.deployed();
+        let totalSupply = await instance.totalSupply();
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.bignumber.equal(totalSupply);
+        expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
+        //we expect the balance to be less one token after sending
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
+        //BN is a javascript class which allows you to make arithmetic operations on big numbers like sub, add, etc
+        //reciptient should have one token
+        expect(instance.balanceOf(recipient)).to.eventually.be.bignumber.equal(new BN(sendTokens));
+
+    })
+
+    it("is not possible to send more tokens than are available in total", async () => {
+        let instance = await Token.deployed();
+        let balanceOfDeployer = await instance.balanceOf(deployerAccount);
+
+        expect(instance.transfer(recipient, new BN(balanceOfDeployer + 1))).to.eventually.be.rejected;
+
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.bignumber.equal(balanceOfDeployer);
     })
 })
